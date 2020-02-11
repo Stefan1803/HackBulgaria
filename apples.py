c8c5dae8090ccb920af8b6faa85a617ca5032a70
Stefan1803 <stefan.toncev123@gmail.com>
2020-02-11 15:07:00 +0200

    rotten apples fix

--- apples.py
+++ apples.py
@@ -1,7 +1,8 @@
 import math
 
-# casket_size = input("Enter the size of the box (example: 20x10): ")
-casket_size = "10x10"
+casket_size = input("Enter the size of the box (example: 20x10): ")
+# casket_size = "5x5"
+# casket_size = "7x7"
 (Nstr, Mstr) = casket_size.strip().split("x")
 N = int(Nstr)
 M = int(Mstr)
@@ -12,9 +13,9 @@ def printCasket():
     for line in casket:
         print(''.join(line))
 
-# cooridantes_input = input("Еnter the coordinates of the rotten apples (example: (3,8) (16,4)): ")
-# cooridantes_input = "(3,4) (6,7)"
-cooridantes_input = "(6,7)"
+cooridantes_input = input("Еnter the coordinates of the rotten apples (example: (3,8) (16,4)): ")
+# cooridantes_input = "(2,2)"
+# cooridantes_input = "(3,3)"
 
 rotten_coordinates = []
 def init_rotten():
@@ -30,33 +31,17 @@ init_rotten()
 days_until_return = int(input("After how many days will you come back: "))
 rotten_range = math.floor(days_until_return / 3)
 
-def markAsRotten(x, y):
-    try:
-        casket[x][y] = "X"
-    except:
-        print("out of range ({},{})".format(x, y))
-
 print("Initial rotten apples:")
 printCasket()
 
 for (x, y) in rotten_coordinates:
     for i in range(1, rotten_range + 1):
-        print(i)
-        markAsRotten(x-i, y-i)
-        markAsRotten(x-i, y)
-        markAsRotten(x-i, y+i)
-        markAsRotten(x, y-i)
-        markAsRotten(x, y)
-        markAsRotten(x, y+i)
-        markAsRotten(x+i, y-i)
-        markAsRotten(x+i, y)
-        markAsRotten(x+i, y+i)
+        for m in range(x - i, x + i + 1):
+            for n in range(x - i, x + i + 1):
+                try:
+                    casket[m][n] = "X"
+                except:
+                    print("out of range ({},{})".format(m, n))
 
 print("Rotten apples after {} days:".format(days_until_return))
 printCasket()
-
-X0X0X
-0XXX0
-XXXXX
-0XXX0
-X0X0X
